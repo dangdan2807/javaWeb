2340f33680e609820405bcc40b3f62e7b575d466
KayJuno <dangdan2807@gmail.com>
2020-11-21 10:35:32 +0700

    hello world

---
+++ src/READMD.md
@@ -0,0 +1,5 @@
+# JAVA WEB - Hello world
+
+- **Model Version:** 4.0.0
+- **Version:** 1.0
+- **Packaging:** war
---
+++ src/main/java/com/laptrinh/controller/admin/Homecontroller.java
@@ -0,0 +1,28 @@
+package com.laptrinh.controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/admin-home"})
+public class Homecontroller extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+}
